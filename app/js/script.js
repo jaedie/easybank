410d11829dc810c41e739fbb32a73fd7a98fc8fa
jaedie <jamesleehw@gmail.com>
2021-03-11 22:44:59 +0900

    update

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,10 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
 
 btnHamburger.addEventListener("click", () => {
-  console.log("open hamburger");
-
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
   } else {
-    btnHamburger.classList.add("open");
+    header.classList.add("open");
   }
 });
